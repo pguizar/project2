e82de443cb313b14279f73e7bf31fd7a638a0c1c
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-01 18:25:34 -0500

    Create questions_db.php

---
+++ questions_db.php
@@ -0,0 +1,17 @@
+<?php
+
+function get_users_questions ($userId) {
+	gloabl $db;
+
+	$query = 'SELECT * FROM questions WHERE ownerid = $userId';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':userId', $userId);
+	$statement->execute();
+
+	$questions = $statement->fetchAll();
+	$statement->closeCursor();
+
+	return $questions;
+}
+
+?>
