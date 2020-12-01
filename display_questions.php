840c74e4efe93c997a058b14cbdb30a4d32eb8eb
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-01 18:23:34 -0500

    Create display_questions.php

---
+++ display_questions.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Display Questions</title>      		
+</head>
+<body>
+  	<table>
+  		<tr>
+  			<th>Name</th>
+  			<th> Body </th>
+
+  		</tr>
+  		<?php foreach ($questions as $question) : ?>
+  			<tr>
+  				<td><?php echo $question['title']; ?></td>
+  				<td><?php echo $question['body']; ?></td>
+  			</tr>
+  		<?php endforeach; ?>	
+  	</table>
+
+</body>
+
+</html>
