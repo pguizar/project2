7f954d63e4da8692cab527cfaa21b896ae77b2b6
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-01 18:22:34 -0500

    Create accounts_db.php

---
+++ accounts_db.php
@@ -0,0 +1,21 @@
+<?php
+
+function validate_login($email, $password) {
+	global $db;
+	$query = 'SELECT * FROM accounts WHERE email = :email AND password = :password';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':email', $email);
+	$statement->bindValue(':password', $password);
+	$statement->execute();
+	$user = $statement->fetch();
+
+	$statement->closeCursor();
+
+	if(count($user) > 0){
+		return $user['id'];
+	}else{
+		return false;
+	}
+
+}
+?>
