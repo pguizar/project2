7dd865f2b360f18e0e79bdc216669d6590bb4eb0
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-01 20:00:00 -0500

    Update display_questions.php

--- display_questions.php
+++ display_questions.php
@@ -4,6 +4,7 @@
 	<title>Display Questions</title>      		
 </head>
 <body>
+  <a href="Location: display_questions&userId=<?php echo $userId; ?>">Add Question</a>
   	<table>
   		<tr>
   			<th>Name</th>
@@ -15,9 +16,11 @@
   				<td><?php echo $question['title']; ?></td>
   				<td><?php echo $question['body']; ?></td>
   			</tr>
+
+
   		<?php endforeach; ?>	
   	</table>
 
-</body> 
+</body>
 
 </html>
