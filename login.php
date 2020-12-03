a2448d4a19ef30bfe7817944e4b666ba0e1c47d7
pguizar <70920704+pguizar@users.noreply.github.com>
2020-12-03 15:44:38 -0500

    Update login.php

--- login.php
+++ login.php
@@ -13,22 +13,22 @@
 			  <li> <a href = "newQuestionForm.php" > New Question Form </a> </li>
 			</ul>
 	</div>
-
+    <div class= "form">
     <form action="index.php" method="post">
         <input type="hidden" name="action" value="validate_login">
 
-        <div class="form-group">
+        
             <label for="email">Email Address</label>
             <input type="email" class="form-control" name="email" id="email">
-        </div>
+        
 
-        <div class="form-group">
             <label for="password">Password</label>
             <input type="password" class="form-control" name="password" id="password">
-        </div>
+ 
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+</div>
 
 </body>
 </html>
